1439b196d8013ff7ddcf0c966b2b893ec4ee1839
Luiz Henrique Ramalho <luizhe.ramalho@gmail.com>
2025-04-23 18:47:46 -0300

    feat: Add product creation test with faker

--- cypress/e2e/gui/products.cy.js
+++ cypress/e2e/gui/products.cy.js
@@ -1,4 +1,6 @@
 // npx cypress run --spec "cypress/e2e/gui/products.cy.js"
+import { faker } from '@faker-js/faker'
+
 describe('Products', () => {
   beforeEach(() => {
     cy.guiLogin()
@@ -7,5 +9,14 @@ describe('Products', () => {
   it('should list all products', () => {
     cy.get("[data-testid='listarProdutos']").click()
     cy.contains('h1', 'Lista dos Produtos').should('be.visible')
+  })
+
+  it('should add a new product', () => {
+    const productName = faker.commerce.productName()
+
+    cy.get("[data-testid='cadastrarProdutos']").click()
+    cy.contains('h1', 'Cadastro de Produtos').should('be.visible')
+    cy.AddProduct(productName, '30', 'Mouse bluetooth preto', '1')
+    cy.url().should('include', '/listarprodutos')
   })
 })
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -8,3 +8,16 @@ Cypress.Commands.add('guiLogin', (
   cy.get("[data-testid='entrar']").click()
   cy.url().should('include', '/home')
 })
+
+Cypress.Commands.add('AddProduct', (productName, productPrice, productDescription, productQuantity) => {
+  cy.fixture('mouse.jpg').as('imageFile')
+  
+  cy.get("[data-testid='nome']").type(productName)
+  cy.get("[data-testid='preco']").type(productPrice)
+  cy.get("[data-testid='descricao']").type(productDescription)
+  cy.get("[data-testid='quantity']").type(productQuantity)
+  cy.get("[data-testid='imagem']").selectFile('@imageFile').should(input => {
+    expect(input[0].files[0].name).to.eq('mouse.jpg')
+  })
+  cy.get("[data-testid='cadastarProdutos']").click()
+})
