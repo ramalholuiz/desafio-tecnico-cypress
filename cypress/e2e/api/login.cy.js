1d06dc4b82b4b1f85c70413bb29ca9fbf6389162
Luiz Henrique Ramalho <luizhe.ramalho@gmail.com>
2025-04-23 22:16:03 -0300

    feat: API login test with invalid credentials

--- cypress/e2e/api/login.cy.js
+++ cypress/e2e/api/login.cy.js
@@ -15,4 +15,19 @@ describe('API Login', () => {
       expect(res.body.message).to.eq('Login realizado com sucesso')
     })
   })
+
+  it('should fail to login with invalid credentials', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://serverest.dev/login',
+      failOnStatusCode: false,
+      body: {
+        email: Cypress.env('invalid_user_email'),
+        password: Cypress.env('invalid_user_password')
+      }
+    }).then((res) => {
+      expect(res.status).to.eq(401)
+      expect(res.body.message).to.eq('Email e/ou senha inválidos')
+    })
+  })
 }) 
